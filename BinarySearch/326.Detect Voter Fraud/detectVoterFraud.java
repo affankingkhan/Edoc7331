736da3e3273d615d9ab4ac68c66df5db5c8b015b
Affan Khan <affank@sfu.ca>
2021-09-25 23:03:07 -0400

    sept 25th 2021

---
+++ BinarySearch/326.Detect Voter Fraud/detectVoterFraud.java
@@ -0,0 +1,14 @@
+// https://binarysearch.com/problems/Detect-Voter-Fraud
+import java.util.*;
+
+class Solution {
+    public boolean solve(int[][] votes) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int[] vote : votes) {
+            if (set.contains(vote[1]))
+                return true;
+            set.add(vote[1]);
+        }
+        return false;
+    }
+}
